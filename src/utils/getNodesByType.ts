05856b7adee70582bb1e9b0ca0502e3af8d4b49e
Nicolás Romani <nicolasromani.dev@gmail.com>
2026-06-27 22:47:43 -0300

    remove unused files

--- src/utils/getNodesByType.ts
+++
@@ -1,6 +0,0 @@
-import type { IPeriodNode } from "../components/PeriodNode"
-import type { IModuleNode } from "../components/ModuleNode"
-
-export default function getNodesByType<T extends IModuleNode | IPeriodNode>(nodes: (IModuleNode | IPeriodNode)[], type: 'module' | 'period'): T[]{
-   return nodes.filter(n => n.type === type).sort((a, b) => a.data.year - b.data.year) as T[]
-}
